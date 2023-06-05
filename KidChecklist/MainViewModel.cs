42320b44fe4afe4c551cce6ffefdacdbe367c53d
Kevin Morris <kmorris511@gmail.com>
2023-06-04 22:26:13 -0600

    Implementing evaluation for all items checked

--- KidChecklist/MainViewModel.cs
+++ KidChecklist/MainViewModel.cs
@@ -8,6 +8,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+// ReSharper disable All
 
 namespace KidChecklist
 {
@@ -17,6 +18,20 @@ namespace KidChecklist
 
         public ObservableCollection<ChecklistItem> Items { get; }
 
+        private bool _isAllChecked;
+        public bool IsAllChecked
+        {
+            get => _isAllChecked;
+            set
+            {
+                if(IsAllChecked != value)
+                {
+                    _isAllChecked = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
         public MainViewModel()
         {
             Items = new ObservableCollection<ChecklistItem>
@@ -24,77 +39,76 @@ namespace KidChecklist
                 new()
                 {
                     Title = "10 minute playroom/bedroom clean-up",
-                    Image = Array.Empty <byte>()
-
+                    Image = Array.Empty<byte>()
                 },
 
                 new()
                 {
                     Title = "Undress",
-                    Image = Array.Empty <byte>()
+                    Image = Array.Empty<byte>()
 
                 },
 
                 new()
                 {
                     Title = "Put dirty clothes in closet",
-                    Image = Array.Empty <byte>()
+                    Image = Array.Empty<byte>()
 
                 },
 
                 new()
                 {
                     Title = "Go potty",
-                    Image = Array.Empty <byte>()
+                    Image = Array.Empty<byte>()
 
                 },
                 
                 new()
                 {
                     Title = "Take bath or shower",
-                    Image = Array.Empty <byte>()
+                    Image = Array.Empty<byte>()
 
                 },
                 
                 new()
                 {
                     Title = "Take Vitamins",
-                    Image = Array.Empty <byte>()
+                    Image = Array.Empty<byte>()
 
                 },
                 
                 new()
                 {
-                    Title ="Brush teeth",
-                    Image = Array.Empty <byte>()
+                    Title = "Brush teeth",
+                    Image = Array.Empty<byte>()
 
                 },
 
                 new()
                 {
                     Title = "Hug brother",
-                    Image = Array.Empty <byte>()
+                    Image = Array.Empty<byte>()
 
                 },
 
                 new()
                 {
                     Title = "Hugs with Mom and Dad",
-                    Image = Array.Empty <byte>()
+                    Image = Array.Empty<byte>()
 
                 },
 
                 new()
                 {
                     Title = "Read books or relax",
-                    Image = Array.Empty <byte>()
+                    Image = Array.Empty<byte>()
 
                 },
 
                 new()
                 {
                     Title = "Go to sleep",
-                    Image = Array.Empty <byte>()
+                    Image = Array.Empty<byte>()
 
                 },
             };
@@ -107,6 +121,7 @@ namespace KidChecklist
 
         private void ChecklistItem_CheckChanged(object sender, EventArgs args)
         {
+            IsAllChecked = Items.All(x => x.IsChecked);
         }
 
         private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
