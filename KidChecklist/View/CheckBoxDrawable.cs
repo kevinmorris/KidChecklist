b45331ddf6c4bd355d9040b57d358d98b3248591
Kevin Morris <kmorris511@gmail.com>
2023-06-05 15:27:32 -0600

    adding checkmark graphic

--- KidChecklist/View/CheckBoxDrawable.cs
+++ KidChecklist/View/CheckBoxDrawable.cs
@@ -1,23 +1,36 @@
-﻿namespace KidChecklist.View
+﻿using System.Reflection;
+using Microsoft.Maui.Graphics.Platform;
+using IImage = Microsoft.Maui.Graphics.IImage;
+
+namespace KidChecklist.View
 {
     public class CheckBoxDrawable : IDrawable
     {
+        private readonly int _size;
+        private readonly IImage _checkMarkImage;
+        public CheckBoxDrawable()
+        {
+            IsChecked = false;
+            App.Current.Resources.TryGetValue("CheckBoxSize", out var sizeObj);
+            _size = (int)sizeObj;
+
+            var assembly = typeof(CheckBoxDrawable).GetTypeInfo().Assembly;
+            using var stream = assembly.GetManifestResourceStream("KidChecklist.Resources.Images.checkmark.png");
+            _checkMarkImage = PlatformImage.FromStream(stream);
+        }
+
         public bool IsChecked { get; set; }
 
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
-            App.Current.Resources.TryGetValue("CheckBoxSize", out var sizeObj);
-            var size = (int)sizeObj;
-
             canvas.StrokeColor = Colors.Black;
             canvas.StrokeSize = 6;
+            canvas.DrawRoundedRectangle(10, 10, _size - 20, _size - 20, 10);
+
             if (IsChecked)
             {
-                canvas.DrawLine(10, 10, size - 10, size - 10);
-                canvas.DrawLine(size - 10, 10, 10, size - 10);
+                canvas.DrawImage(_checkMarkImage, 0, 0, _size, _size);
             }
-
-            canvas.DrawRoundedRectangle(10, 10, size - 10, size - 10, 10);
         }
     }
 }
